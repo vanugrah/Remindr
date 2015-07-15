c1be77ed6b6ba717be9a12d7b74b1d7840a5bc4f
Anugrah Vijay <cooldude_anugrah@hotmail.com>
2015-07-15 23:53:19 +0700

    Adds light landing page and links it to the login page. Both with reduced css stylesheets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # Need CAS Authorization to access main app
-	before_filter CASClient::Frameworks::Rails::Filter
 
-  def hello
-
-  	 render text: "Hello World!"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-
-  get  'login'   => 'application#hello'
- 
+ 	
 end
